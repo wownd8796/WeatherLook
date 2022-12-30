9277d361a8d7b6cd844ee0b7baf659c78c4b919d
minsuh Jo <zziboo3773@gmail.com>
2022-12-30 18:20:28 +0900

    weather Look main

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,8 @@
 window.addEventListener("load", () => {
   //   console.log("test");
+  const mainBox = document.querySelector(".mainBox");
+  setTimeout(() => {
+    mainBox.classList.add("animate_fadeIn");
+    mainBox.style.opacity = 1;
+  }, 500);
 });
